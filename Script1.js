83cf2f87bf7a8701e3558e1e50f5f49cc9de9466
P-Millard <parismillard9001@gmail.com>
2024-04-21 21:51:39 +0100

    Button Changes

--- Script1.js
+++ Script1.js
@@ -1,2 +1,14 @@
 // JavaScript source code
 
+function add()
+{
+    const inputArray = [];
+
+    const input = document.getElementById("input");
+
+    const value = input.value;
+
+    inputArray.push(value);
+
+    console.log(inputArray);
+}
