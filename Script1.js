0b81db5490659a5042cabd393379cd3d739b9da7
P-Millard <parismillard9001@gmail.com>
2024-04-21 23:23:54 +0100

    Save For Break

--- Script1.js
+++ Script1.js
@@ -1,15 +1,1 @@
 // JavaScript source code
-
-function add()
-{
-    const input = document.getElementById("input");
-
-    const value = input.value;
-
-    data.push(value);
-}
-
-function remove()
-{
-
-}
